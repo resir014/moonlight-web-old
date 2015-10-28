acf81dbe5b547569f34f63b1bcda53ccb0ec382d
Resi Respati <resir014@gmail.com>
2015-10-28 23:17:40 +0700

    Now Playing & play history now works (thanks @puckipedia!)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,38 @@
-// Initialise JQuery SHOUTcast
-$.SHOUTcast({
-  host: 'starlightcomic.ddns.net',
-  port: 8000,
-  interval: 5000,
-  if(this.onAir()){
-    $('#songtitle').text(this.get('songtitle'));
-  } else {
-    $('#songtitle').text(this.get('songtitle','Off Air'));
-  }
-}).startStats();
+// Icecast stream details thingy. (Thanks @puckipedia!)
+
+var previousSongs = [];
+var currentSong = "";
+
+function sum(item, predicate) {
+  var total = 0;
+  for (var i in item)
+    total += predicate(item[i], i);
+  return total;
+}
+
+setInterval(function() {
+  $.getJSON("http://moonlightradio.ddns.net:8000/status-json.xsl", function(data) {
+    var stream_data = data.icestats.source[0];
+    if (!("artist" in stream_data))
+      stream_data.artist = "";
+    else
+      stream_data.artist += " - ";
+    if (!("title" in stream_data))
+      stream_data.title = "<unknown>";
+    var current_song = stream_data.artist + stream_data.title;
+    if (current_song != currentSong) {
+      previousSongs.unshift(current_song);
+      if (previousSongs.length > 10)
+        previousSongs = previousSongs.slice(0, 10);
+      $("#current-track").text(current_song);
+      var last_played = $("#played").empty();
+      for (var song_i in previousSongs)
+        $("<li />").text(previousSongs[song_i]).appendTo(last_played);
+      currentSong = current_song;
+    }
+    $("#current-listeners").text(
+      sum(data.icestats.source, function(a) { return a.listeners; }) + "/" +
+      sum(data.icestats.source, function(a) { return a.listener_peak; }));
+    $("#maxlisteners").text(stream_data.listener_peak);
+  });
+}, 1000);
