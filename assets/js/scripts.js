83bdf1ead3d2756e3bf2c2e59d0bb5a57473dda1
Resi Respati <resir014@gmail.com>
2015-10-29 17:16:43 +0700

    Script cleanups
    * Changed double quote marks to single quote marks.
    * Reworked how peak listeners are displayed.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 // Icecast stream details thingy. (Thanks @puckipedia!)
 
 var previousSongs = [];
-var currentSong = "";
+var currentSong = '';
 
 function sum(item, predicate) {
   var total = 0;
@@ -11,27 +11,29 @@ function sum(item, predicate) {
 }
 
 setInterval(function() {
-  $.getJSON("http://moonlightradio.ddns.net:8000/status-json.xsl", function(data) {
+  $.getJSON('http://moonlightradio.ddns.net:8000/status-json.xsl', function(data) {
     var stream_data = data.icestats.source[0];
-    if (!("artist" in stream_data))
-      stream_data.artist = "";
+    if (!('artist' in stream_data))
+      stream_data.artist = '';
     else
-      stream_data.artist += " - ";
-    if (!("title" in stream_data))
-      stream_data.title = "<unknown>";
+      stream_data.artist += ' - ';
+    if (!('title' in stream_data))
+      stream_data.title = '<unknown>';
     var current_song = stream_data.artist + stream_data.title;
     if (current_song != currentSong) {
       previousSongs.unshift(current_song);
       if (previousSongs.length > 10)
         previousSongs = previousSongs.slice(0, 10);
-      $("#songtitle").html(current_song);
-      var last_played = $("#played").empty();
+      $('#songtitle').html(current_song);
+      var last_played = $('#played').empty();
       for (var song_i in previousSongs)
-        $("<li />").html(previousSongs[song_i]).appendTo(last_played);
+        $('<li />').html(previousSongs[song_i]).appendTo(last_played);
       currentSong = current_song;
     }
-    $("#currentlisteners").text(
-      sum(data.icestats.source, function(a) {return a.listeners; }) + "/" +
+    $('#currentlisteners').text(
+      sum(data.icestats.source, function(a) {return a.listeners; })
+    );
+    $('#maxlisteners').text(
       sum(data.icestats.source, function(a) { return a.listener_peak; })
     );
   });
