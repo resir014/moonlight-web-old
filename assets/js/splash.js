f11a39f1b3c9a8e870a300673fa524c6c05851b4
Resi Respati <resir014@gmail.com>
2015-11-18 00:28:36 +0700

    Double quotemarks to single quotemarks

--- assets/js/splash.js
+++ assets/js/splash.js
@@ -3,27 +3,27 @@
  */
 (function() {
   var splash = [
-    "moonlightradio.ddns.net",
-    "Like memes? Join MOONLIGHT!",
-    "Home of the dankest memes and the angriest sergals.",
-    "Better a hole in your moon, than a moon in your hole.",
-    "Where were you, when STARLIGHT is kill?",
-    "Held together with duct tape.",
-    "We cook on ion drives.",
-    "Very vaporwave, much cloudy.",
-    "We excel at crashing into asteroids.",
-    "We welcome everyone (even sergals!)",
-    "The dark side of STARLIGHT.",
-    "Playing nothing but Wolfgun since 2015.",
-    "Powered by Winamp.",
-    "Bringing you the most diverse tunes from like 5 people.",
-    "リサフランク420 / 現代のコンピュー",
-    "Nothing but vaporwave and Wolfgun.",
-    "God is dead, we have killed him with vaporwave.",
-    "Sergals. That is all.",
-    "<+Penumbra> MEMES FUEL THE WORLD",
-    "Everything is possible!"
+    'moonlightradio.ddns.net',
+    'Like memes? Join MOONLIGHT!',
+    'Home of the dankest memes and the angriest sergals.',
+    'Better a hole in your moon, than a moon in your hole.',
+    'Where were you, when STARLIGHT is kill?',
+    'Held together with duct tape.',
+    'We cook on ion drives.',
+    'Very vaporwave, much cloudy.',
+    'We excel at crashing into asteroids.',
+    'We welcome everyone (even sergals!)',
+    'The dark side of STARLIGHT.',
+    'Playing nothing but Wolfgun since 2015.',
+    'Powered by Winamp.',
+    'Bringing you the most diverse tunes from like 5 people.',
+    'リサフランク420 / 現代のコンピュー',
+    'Nothing but vaporwave and Wolfgun.',
+    'God is dead, we have killed him with vaporwave.',
+    'Sergals. That is all.',
+    '<+Penumbra> MEMES FUEL THE WORLD',
+    'Everything is possible!'
   ];
   var rand = splash[Math.floor(Math.random() * splash.length)];
-  document.getElementById("splash").innerText = rand;
+  document.getElementById('splash').innerText = rand;
 })();
